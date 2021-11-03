95fe59100eeda0171283435b57f8dac1a6a91203
Carissa <Carissa.Rousseau@ghd.com>
2021-11-03 16:01:40 -0400

    html/css cards!

---
+++ src/js/init.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		items: [
+			{
+				id:"1294",
+				title: 'Wicked Goblin Tribe Spear & Shield',
+				description: 'Medium sizes Golbin from Green Siege set 35 from Epic Minitures',
+				tags: ['Epic Minitures', '35', 'Green Siege'],
+				img: 'images/1294 - Wicked Goblin Tribe Spear & Shield (Medium).png',
+				source: 'Epic Minitures'
+			},
+			{
+				id:"1295",
+				title: '1295 - Wicked Goblin Tribe Archer',
+				description: 'Medium sizes Golbin from Green Siege set 35 from Epic Minitures',
+				tags: ['Epic Minitures', '35', 'Green Siege'],
+				img: 'images/1295 - Wicked Goblin Tribe Archer (Medium).png',
+				source: 'Epic Minitures'
+			}
+		]
+	}
+  })
